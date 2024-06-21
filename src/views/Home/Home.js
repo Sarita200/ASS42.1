a672abcbf7458cbeb2286e309263aff4076dae16
Sarita200 <saritasadgir1@gmail.com>
2024-06-21 21:22:00 +0530

    Changes in cards

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -6,7 +6,7 @@ function Home() {
     console.log(blogs);
   return (
     <>
-    <h1 className="Heading">RJ's Blogs</h1>
+    <h1 className="Heading">Movie's Blogs</h1>
         <div className="blogs-container">
             {
                 blogs.map((blogObject, i) => {
